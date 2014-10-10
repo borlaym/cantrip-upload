c3b098da1b39a3a73e9e88199d164fc3b91ca94c
Márton Borlay <borlaym@Martons-MacBook-Pro.local>
2014-10-10 14:34:35 +0200

    No more cantrip dep

--- server.js
+++ server.js
@@ -1,21 +1,7 @@
 var Cantrip = require("Cantrip");
-var auth = require("cantrip-auth");
 var fs = require("fs");
 var upload = require("./index.js");
 
-Cantrip.use(auth);
 
 Cantrip.special("/upload", upload);
-
-Cantrip.options.https = {
-	key: fs.readFileSync(process.env["HOME"] + '/.canvas/server.key', 'utf8'),
-	cert: fs.readFileSync(process.env["HOME"] + '/.canvas/server.crt', 'utf8')
-};
-
-Cantrip.special("/login", function(req, res, next) {
-	res.body.id = req.body._id;
-	next();
-});
-
-
 Cantrip.start();
